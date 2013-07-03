f11cc744df79919cd52105625567b73fb0f7a050
Jeroen Serpieters <jeroen@serpieters.be>
2013-07-04 03:54:19 +0530

    Update repository url to https

--- provisioning/virtualbox.sh
+++ provisioning/virtualbox.sh
@@ -4,7 +4,7 @@ yum -y install virt-what
 
 if [ `virt-what` == "virtualbox" ]; then
   yum -y install kernel-devel kernel-headers gcc make
-  rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
   yum -y install dkms
   export KERN_DIR=/usr/src/kernels/`uname -r`
   mkdir /mnt/VBoxGuestAdditions
