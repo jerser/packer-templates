28bc1db05c0f22701d0d4473269458a00f9e6278
Jeroen Serpieters <jeroen@serpieters.be>
2013-07-04 14:58:15 +0530

    Added Vagrant provisioning

---
+++ provisioning/vagrant.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ `virt-what` == "virtualbox" ]; then
+  date > /etc/vagrant_box_build_time
+
+  useradd -m vagrant
+  echo -e "vagrant\nvagrant" | (passwd --stdin vagrant)
+
+  echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+  mkdir -pm 700 /home/vagrant/.ssh
+  curl -L https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
+  chmod 0600 /home/vagrant/.ssh/authorized_keys
+  chown -R vagrant.vagrant /home/vagrant/.ssh
+fi
